4f3cec391452e362cbfd8d441017ebf7304ad42f
Dese Narfa Firmansyah <desenfirman@GMAIL.COM>
2019-11-24 10:36:45 +0700

    some code refactoring in gatsby-node.js

--- react-gatsby/desenfirman.github.io/gatsby-node.js
+++ react-gatsby/desenfirman.github.io/gatsby-node.js
@@ -7,6 +7,76 @@
 // You can delete this file if you're not using it
 
 // Create blog post list pages
+
+function createPostPages(createPage, posts, postsPerPage) {
+  const blogPost = path.resolve(`./src/pages/blog-post.js`)
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    })
+  })
+
+  // Create blog post list pages
+  const numPages = Math.ceil(posts.length / postsPerPage);
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? '/blog' + `/` : '/blog' + `/${i + 1}`,
+      component: path.resolve('./src/pages/blog-list.js'),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1
+      },
+    });
+  });
+}
+
+function createPortfolioPages(createPage, posts, portfolioPerPage) {
+  const portfolioPost = path.resolve(`./src/pages/portfolio-post.js`)
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+    
+    createPage({
+      path: '/portfolio/' + post.node.name,
+      component: portfolioPost,
+      context: {
+        name: post.node.name,
+        previous,
+        next,
+      },
+    })
+  })
+  
+  // Create portfolio post list pages
+  const numPages = Math.ceil(posts.length / portfolioPerPage);
+  
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? '/portfolio' + `/` : '/portfolio' + `/${i + 1}`,
+      component: path.resolve('./src/pages/portfolio-list.js'),
+      context: {
+        limit: portfolioPerPage,
+        skip: i * portfolioPerPage,
+        numPages,
+        currentPage: i + 1
+      },
+    });
+  });
+}
+
+
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
@@ -14,7 +84,6 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   
 
-  const blogPost = path.resolve(`./src/pages/blog-post.js`)
   return graphql(
     `
       {
@@ -33,59 +102,6 @@ exports.createPages = ({ graphql, actions }) => {
             }
           }
         }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
-
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-
-    // Create blog post list pages
-    const postsPerPage = 2;
-    const numPages = Math.ceil(posts.length / postsPerPage);
-
-    Array.from({ length: numPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? '/blog' + `/` : '/blog' + `/${i + 1}`,
-        component: path.resolve('./src/pages/blog-list.js'),
-        context: {
-          limit: postsPerPage,
-          skip: i * postsPerPage,
-          numPages,
-          currentPage: i + 1
-        },
-      });
-    });
-  })
-}
-
-// Portfolio Entry
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  
-  
-  const portfolioPost = path.resolve(`./src/pages/portfolio-post.js`)
-  return graphql(
-    `
-      {
         allGithubData(
           limit: 1000
           ) {
@@ -110,53 +126,27 @@ exports.createPages = ({ graphql, actions }) => {
           }
         }
       }
-      `
+    `
   ).then(result => {
     if (result.errors) {
       throw result.errors
     }
-    
+
     // Create blog posts pages.
-    const posts = result.data.allGithubData.edges[0].node.data.search.edges
-    console.log(posts)
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-      
-      createPage({
-        path: '/portfolio/' + post.node.name,
-        component: portfolioPost,
-        context: {
-          name: post.node.name,
-          previous,
-          next,
-        },
-      })
-    })
-    
-    // Create blog post list pages
-    const portfolioPerPage = 2;
-    const numPages = Math.ceil(posts.length / portfolioPerPage);
-    
-    Array.from({ length: numPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? '/portfolio' + `/` : '/portfolio' + `/${i + 1}`,
-        component: path.resolve('./src/pages/portfolio-list.js'),
-        context: {
-          limit: portfolioPerPage,
-          skip: i * portfolioPerPage,
-          numPages,
-          currentPage: i + 1
-        },
-      });
-    });
+    const posts = result.data.allMarkdownRemark.edges
+    createPostPages(createPage, posts, 2)
+
+
+    // Create portfolio posts pages.
+    const portfolios = result.data.allGithubData.edges[0].node.data.search.edges
+    createPortfolioPages(createPage, portfolios, 2)
   })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark` ) {
+  if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
